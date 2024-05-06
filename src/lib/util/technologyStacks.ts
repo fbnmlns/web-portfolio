94918c73ab0ecad3dae94e8bef5257c7963f10e0
fbnmlnss <fabimilanes@gmail.com>
2024-05-06 13:38:50 -0600

    feat: reordered the tags on project cards

--- src/lib/util/technologyStacks.ts
+++ src/lib/util/technologyStacks.ts
@@ -1,3 +1,3 @@
-export const bloomStack = ["Spring Boot", "Angular", "MySQL", "AWS"];
+export const bloomStack = ["Angular", "Spring Boot", "MySQL", "AWS"];
 export const mokaStack = ["HTML", "CSS", "JavaScript", "MongoDB", "Express.js"];
-export const echowaveStack = ["Spring Boot", "JavaFX", "CSS", "MySQL"];
+export const echowaveStack = ["JavaFX", "CSS", "Spring Boot", "MySQL"];
